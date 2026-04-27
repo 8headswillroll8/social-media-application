8972d4244cd6fd3607f0e43bc5333c0ce33d023d
8headswillroll8 <lejohansen91@gmail.com>
2026-04-27 21:28:55 +0200

    style: add layout and styles for register page

---
+++ js/pages/register.js
@@ -0,0 +1,12 @@
+const email = document.getElementById("email");
+const emailFeedback = document.getElementById("email-feedback");
+
+const password = document.getElementById("password");
+const passwordFeedback = document.getElementById("password-feedback");
+
+const confirmPassword = document.getElementById("confirm-password");
+const confirmPasswordFeedback = document.getElementById(
+  "confirm-password-feedback",
+);
+
+const button = document.getElementById("login-button");
