0b4c38f3d4922c426af09520ca8edf1a40de2391
8headswillroll8 <lejohansen91@gmail.com>
2026-04-28 11:26:47 +0200

    Add register form validation

--- js/pages/register.js
+++ js/pages/register.js
@@ -1,3 +1,5 @@
+const form = document.querySelector(".register__form");
+
 const email = document.getElementById("email");
 const emailFeedback = document.getElementById("email-feedback");
 
@@ -9,4 +11,68 @@ const confirmPasswordFeedback = document.getElementById(
   "confirm-password-feedback",
 );
 
-const button = document.getElementById("login-button");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const validateInputs = () => {
+  emailFeedback.innerText = "";
+  emailFeedback.classList.remove("txt--error");
+  email.classList.remove("has-error");
+
+  passwordFeedback.innerText = "";
+  passwordFeedback.classList.remove("txt--error");
+  password.classList.remove("has-error");
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const confirmPasswordValue = confirmPassword.value.trim();
+
+  let isValid = true;
+
+  if (emailValue === "") {
+    emailFeedback.innerText = "Skriv inn e-post.";
+    emailFeedback.classList.add("txt--error");
+    email.classList.add("has-error");
+
+    isValid = false;
+  }
+
+  if (emailValue !== "" && !emailValue.includes("@")) {
+    emailFeedback.innerText = "Ugyldig e-postadresse.";
+    emailFeedback.classList.add("txt--error");
+
+    isValid = false;
+  }
+
+  if (passwordValue === "") {
+    passwordFeedback.innerText = "Skriv inn passord.";
+    passwordFeedback.classList.add("txt--error");
+    password.classList.add("has-error");
+
+    isValid = false;
+  }
+
+  if (passwordValue !== "" && passwordValue.length < 8) {
+    passwordFeedback.innerText = "Minst 8 tegn.";
+    passwordFeedback.classList.add("txt--error");
+    password.classList.add("has-error");
+
+    isValid = false;
+  }
+
+  if (passwordValue !== confirmPasswordValue) {
+    confirmPasswordFeedback.innerText = "Passordene må være like.";
+    confirmPasswordFeedback.classList.add("txt--error");
+    confirmPassword.classList.add("has-error");
+    password.classList.add("has-error");
+
+    isValid = false;
+  }
+
+  if (isValid) {
+    window.location.href = "/account/login.html?registered=true";
+  }
+};
