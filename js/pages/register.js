80ecb02ab90766c3e7b2e49445587f9738f32631
8headswillroll8 <lejohansen91@gmail.com>
2026-04-28 16:28:34 +0200

    Add username field to register form

--- js/pages/register.js
+++ js/pages/register.js
@@ -1,4 +1,9 @@
+import { post } from "../api/apiClient.js";
+
 const form = document.querySelector(".register__form");
+
+const username = document.getElementById("username");
+const usernameFeedback = document.getElementById("username-feedback");
 
 const email = document.getElementById("email");
 const emailFeedback = document.getElementById("email-feedback");
@@ -11,13 +16,19 @@ const confirmPasswordFeedback = document.getElementById(
   "confirm-password-feedback",
 );
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  validateInputs();
+  const isValid = validateInputs();
+
+  if (!isValid) return;
 });
 
 const validateInputs = () => {
+  usernameFeedback.innerText = "";
+  usernameFeedback.classList.remove("txt--error");
+  username.classList.remove("has-error");
+
   emailFeedback.innerText = "";
   emailFeedback.classList.remove("txt--error");
   email.classList.remove("has-error");
@@ -26,11 +37,38 @@ const validateInputs = () => {
   passwordFeedback.classList.remove("txt--error");
   password.classList.remove("has-error");
 
+  const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const confirmPasswordValue = confirmPassword.value.trim();
 
   let isValid = true;
+
+  if (usernameValue === "") {
+    usernameFeedback.innerText = "Skriv inn et brukernavn";
+    usernameFeedback.classList.add("txt--error");
+    username.classList.add("has-error");
+
+    isValid = false;
+  }
+
+  if (usernameValue !== "" && usernameValue.length < 3) {
+    usernameFeedback.innerText = "Minst 3 tegn.";
+    usernameFeedback.classList.add("txt--error");
+    username.classList.add("has-error");
+  }
+
+  if (usernameValue.includes(" ")) {
+    usernameFeedback.innerText = "Kan ikke inneholde mellomrom.";
+    usernameFeedback.classList.add("txt--error");
+    username.classList.add("has-error");
+  }
+
+  if (usernameValue.length > 20) {
+    usernameFeedback.innerText = "Maks 20 tegn.";
+    usernameFeedback.classList.add("txt--error");
+    username.classList.add("has-error");
+  }
 
   if (emailValue === "") {
     emailFeedback.innerText = "Skriv inn e-post.";
