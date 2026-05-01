9617ff33bb38c6c9ec4d4f0354497b774f661620
8headswillroll8 <lejohansen91@gmail.com>
2026-05-01 11:34:01 +0200

    Fetch and render single post using ID from URL

--- js/pages/post.js
+++ js/pages/post.js
@@ -5,52 +5,54 @@ initHeader();
 
 const feed = document.querySelector(".feed");
 
-async function getPosts() {
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+async function getPost() {
   try {
-    const response = await get("/social/posts?_author=true");
+    if (!id) {
+      feed.innerHTML = `<p class="txt--help">Fant ikke innlegget.</p>`;
+      return;
+    }
 
-    const posts = response.data;
+    const response = await get(
+      `/social/posts/${id}?_author=true&_comments=true`,
+    );
 
-    renderPosts(posts);
+    const post = response.data;
+
+    renderPost(post);
   } catch (error) {
     console.error(error);
     feed.innerHTML = `<p class="txt--help">Kunne ikke laste innlegg.</p>`;
   }
 }
 
-function renderPosts(posts) {
-  if (posts.length === 0) {
-    feed.innerHTML = `<p class="txt--help">Ingen innlegg enda.</p>`;
-    return;
-  }
-
-  const postCards = posts.map((post) => {
-    return `
+function renderPost(post) {
+  const postCard = `
 <article class="card">
-<div class="card__header">
-<img
-class="card__avatar"
-src="${post.author.avatar?.url || "../assets/images/users/user-default-avatar.webp"}"
-alt="${post.author.avatar?.alt || post.author.name}"
-/>
-<div class="card__author">
-<p class="card__username">${post.author.name}</p>
-<time datetime="${post.created}">${post.created}</time>
-</div>
-<button class="btn-follow btn-outline" type="button">
-Følger
-</button>
-</div>
+  <div class="card__header">
+    <img
+      class="card__avatar"
+      src="${post.author.avatar?.url || "../assets/images/users/user-default-avatar.webp"}"
+      alt="${post.author.avatar?.alt || post.author.name}"
+    />
+    <div class="card__author">
+      <p class="card__username">${post.author.name}</p>
+      <time datetime="${post.created}">${post.created}</time>
+    </div>
+    <button class="btn-follow btn-outline" type="button">
+      Følger
+    </button>
+  </div>
 
-<div class="post__like-anim">s
-<img src="../assets/graphics/icons/icon-heart.svg" alt="" />
-</div>
+  <div class="post__like-anim">
+    <img src="../assets/graphics/icons/icon-heart.svg" alt="" />
+  </div>
 
-<a href="./post.html?id=${post.id}">
   <div class="card__content">
     <img src="${post.media?.url || "../assets/images/content/content-fallback.webp"}" alt="${post.media?.alt || ""}" />
   </div>
-  </a>
 
   <div class="card__actions">
     <button class="post__like" type="button" aria-label="Lik innlegget">
@@ -74,9 +76,8 @@ Følger
   </div>
 </article>
 `;
-  });
 
-  feed.innerHTML = postCards.join("");
+  feed.innerHTML = postCard;
 }
 
-getPosts();
+getPost();
